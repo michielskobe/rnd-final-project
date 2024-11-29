f8f612f441e15efe0a6220c86b21ae6b0f283369
Wout Lyen <woutlyen@gmail.com>
2024-11-29 03:06:17 +0100

    Added working ring modulator and testbench
    But the fifo overflows because the xpm fifo is not doing it's job properly

---
+++ filters/VHDL/ring_modulation/tb_ring_modulation.vhd
@@ -0,0 +1,117 @@
+----------------------------------------------------------------------------------
+-- Company: KUL - rnd embed - Beats N Bytes
+-- Engineer: Wout Lyen
+-- 
+-- Create Date: 11/22/2024
+-- Design Name: test bench biquad
+-- Module Name: tb_ring_modulation - Behavioral
+-- Project Name: Blendinator
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+use IEEE.NUMERIC_STD.ALL;
+use ieee.MATH_REAL.all;
+use ieee.STD_LOGIC_UNSIGNED.all;
+
+use std.env.stop;
+
+
+library work;
+use work.wav2axi;
+use work.axi2wav;
+use work.axi4_audio_pkg.all;
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_ring_modulation is
+--  Port ( );
+end tb_ring_modulation;
+
+architecture Behavioral of tb_ring_modulation is
+    ------------------------------------
+    -- Clock
+    ------------------------------------
+    signal clk : STD_LOGIC := '0';
+    signal clk_period : time := 10 ns;
+
+    ------------------------------------
+    -- AXI streams
+    ------------------------------------
+    signal wav_axi_fwd : t_axi4_audio_fwd := (TID => (others => '0'), TData => (others => '0'), TValid => '0', TLast =>'0');
+    signal wav_axi_bwd : t_axi4_audio_bwd := (TReady => '1');
+
+    signal axi_wav_fwd : t_axi4_audio_fwd := (TID => (others => '0'), TData => (others => '0'), TValid => '0', TLast =>'0');
+    signal axi_wav_bwd : t_axi4_audio_bwd := (TReady => '1');
+
+    signal bp_period : time := 100000 ns;
+
+begin
+
+    --clock
+    clk <= not clk after clk_period/2;
+    -- wav_axi_bwd.TReady <= not wav_axi_bwd.TReady after bp_period;
+
+     -- device under test
+     i_dut: entity wav2axi
+     generic map(
+        g_file_name => "sine_sweep.wav",
+        g_channel => 0,
+        g_start_del => 10
+     )
+     port map(
+         -- input
+         ratio => 256,
+         clk_in => clk,
+         
+         -- output
+         axi_out_fwd => wav_axi_fwd,
+         axi_out_bwd => wav_axi_bwd
+     );
+
+     
+     ring_modulator_inst: entity work.ring_modulator
+      generic map(
+         g_TID_count => 1,
+         g_chip_scope => "False"
+     )
+      port map(
+         clk => clk,
+         axi_in_fwd => wav_axi_fwd,
+         axi_in_bwd => wav_axi_bwd,
+         axi_out_fwd => axi_wav_fwd,
+         axi_out_bwd => axi_wav_bwd
+     );
+
+     -- device under test
+     i_dut_2: entity axi2wav
+     generic map(
+        g_file_name => "sine_sweep_out_ring_mod2.wav",
+        g_channel => 0 
+     )
+     port map(
+         -- input
+         ratio => 256,
+         clk_in => clk,
+         
+         -- output
+         axi_in_fwd => axi_wav_fwd,
+         axi_in_bwd => axi_wav_bwd
+     );
+
+
+end Behavioral;
